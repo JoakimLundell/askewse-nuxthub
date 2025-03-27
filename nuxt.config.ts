3147fb3fad51c75c31d8c7e8e8adcb5d5e84fbcb
Joakim Lundell <jocke@bluestripes.se>
2025-03-27 21:55:05 +0100

    Test 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,12 +8,12 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   // Env variables - https://nuxt.com/docs/getting-started/configuration#environment-variables-and-private-tokens
-  runtimeConfig: {
+  /*runtimeConfig: {
     public: {
       // Can be overridden by NUXT_PUBLIC_HELLO_TEXT environment variable
       helloText: "Hello from the Edge 👋",
     },
-  },
+  },*/
   // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
   future: { compatibilityVersion: 4 },
   compatibilityDate: "2025-03-01",
