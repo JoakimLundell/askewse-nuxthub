22f79b5552c7b5786135df3960c5e6859f29c826
Joakim Lundell <jocke@bluestripes.se>
2025-03-27 21:16:00 +0100

    Add logout endpoint

---
+++ server/api/auth/logout.delete.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+export default defineEventHandler(async (event) => {
+  // Get user id
+  const id = event.context.id;
+  if (id) {
+    // Remove token from db
+    const db = hubDatabase();
+    await db
+      .prepare("UPDATE users SET token = null WHERE id = ?1")
+      .bind(id)
+      .run();
+  }
+  // Remove cookie
+  setCookie(event, "askew_token", null);
+  // Return
+  return { success: true };
+});
