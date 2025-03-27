ec3e4cbad335c299d34998bc2d4a92b79faea7d7
Joakim Lundell <jocke@bluestripes.se>
2025-03-27 21:12:47 +0100

    Init auth store check in mounted

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,30 @@
 <template>
-  <NuxtRouteAnnouncer />
-  <NuxtLoadingIndicator />
-  <NuxtPage />
+  <UApp>
+    <AskewHeader />
+    <NuxtPage />
+  </UApp>
 </template>
+
+<script setup>
+import { useAuthStore } from "./store/auth";
+
+onMounted(() => {
+  const authStore = useAuthStore();
+  authStore.init();
+});
+</script>
+
+<style>
+html {
+  scroll-behavior: smooth;
+  scroll-snap-type: y mandatory;
+}
+
+section {
+  scroll-snap-align: start;
+  scroll-margin-top: var(--header);
+  height: calc(100vh - 64px);
+  width: 100%;
+  overflow: hidden;
+}
+</style>
